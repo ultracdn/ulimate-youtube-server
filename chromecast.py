58b3c0d1e1f961c0532c844825c0b9a641856914
Raymond <20248577+javaarchive@users.noreply.github.com>
2019-04-23 11:21:16 -0700

    Update chromecast.py

--- chromecast.py
+++ chromecast.py
@@ -1,22 +1,24 @@
-import pychromecast
-import pafy
-chromecasts = pychromecast.get_chromecasts()
-for x in chromecasts:
-    x.wait()
+#import pychromecast
+#import pafy
+#chromecasts = pychromecast.get_chromecasts()
+#for x in chromecasts:
+#   x.wait()
 def pause():
-    for x in chromecasts:
-        x.media_controller.pause()
+    pass
+#    for x in chromecasts:
+#        x.media_controller.pause()
 def play():
-    for x in chromecasts:
-        x.media_controller.play()
+    pass
+#    for x in chromecasts:
+#        x.media_controller.play()
 def yt(url):
-    v=pafy.new(url)
-    aud = v.getbest(preftype="mp4")
-    #print(v.audiostreams)
-    u=aud.url
-    for x in chromecasts:
-        x.media_controller.play_media(u,"video/mp4")
-def vol(i):
-    for x in chromecasts:
-        x.set_volume(i)
+    pass
+#   v=pafy.new(url)
+#   aud = v.getbest(preftype="mp4")
+#   #print(v.audiostreams)
+#   u=aud.url
+#   for x in chromecasts:
+#       x.media_controller.play_media(u,"video/mp4")
+#def vol(i):
+    pass
         
